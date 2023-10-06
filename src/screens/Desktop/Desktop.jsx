d683d2df0ed1a6d52add719151757c3e587d74e1
hiIamShiba <23127101@student.hcmus.edu.vn>
2023-10-06 14:54:24 +0700

    first commit

---
+++ src/screens/Desktop/Desktop.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="overlap">
+          <div className="text-wrapper">PHONG JOURNY</div>
+          <img className="laughing" alt="Laughing" src="/img/laughing-1.png" />
+          <div className="text-wrapper-2">INFORMATION WEBSITE</div>
+          <div className="mask-group" />
+        </div>
+        <div className="frame">
+          <div className="overlap-group">
+            <a href="https://github.com/hiIamShiba" target="_blank">
+              <img className="github" alt="Github" src="/img/github-1.svg" />
+            </a>
+            <a href="https://linkedin.com/in/phongtu3" target="_blank">
+              <img className="linkedin" alt="Linkedin" src="/img/linkedin-1.svg" />
+            </a>
+            <img className="phone" alt="Phone" src="/img/phone-1.svg" />
+            <div className="text-wrapper-3">(+84) 0365691421</div>
+            <img className="img" alt="Mask group" src="/img/mask-group.png" />
+            <div className="text-wrapper-4">tuthephong4629@gmail.com</div>
+            <a href="https://www.facebook.com/phong.journy.7/" target="_blank">
+              <img className="facebook" alt="Facebook" src="/img/facebook-1-1.png" />
+            </a>
+          </div>
+        </div>
+        <div className="overlap-2">
+          <img className="final" alt="Final" src="/img/final-1-1.png" />
+          <img className="HCMUS" alt="Hcmus" src="/img/hcmus-1.png" />
+          <img className="image" alt="Image" src="/img/image-1.png" />
+          <img className="image-2" alt="Image" src="/img/image-2.png" />
+          <a
+            className="text-wrapper-5"
+            href="https://drive.google.com/file/d/1eTcZ_750OLSoke_qrZWFkD4EAZQ3Lsj7/view?usp=sharing"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            CLICK HERE
+          </a>
+          <a
+            className="text-wrapper-6"
+            href="https://drive.google.com/file/d/1U-gPWqLSRde_DFhlXDrWbEocPObZb5Aa/view?usp=sharing"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            CLICK HERE
+          </a>
+          <a
+            className="text-wrapper-7"
+            href="https://drive.google.com/file/d/1XPeL2o1CN0cBt1ZiXJrCTK-6qDFjvjfV/view?usp=sharing"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            CLICK HERE
+          </a>
+          <a
+            className="text-wrapper-8"
+            href="https://drive.google.com/file/d/1McEiaPo7WtF67i1gWwXU26UCTiLX2c5o/view?usp=sharing"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            CLICK HERE
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
